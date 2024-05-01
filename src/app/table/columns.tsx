b79308625b6ddf2853738fdbac059e91a79cf20e
SAHIL PRASAD <sahilprasad033@gmail.com>
2024-05-01 21:33:12 +0530

    table ui changes

--- src/app/table/columns.tsx
+++ src/app/table/columns.tsx
@@ -2,6 +2,8 @@
 
 import {ColumnDef} from "@tanstack/react-table"
 import Image from "next/image";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+import {Button} from "@/components/ui/button";
 
 export type Pokimons = {
 	id: string
@@ -30,16 +32,40 @@ export const columns: ColumnDef<Pokimons>[] = [
 	},
 	{
 		accessorKey: "name",
-		header: () => <div className="text-left">Name</div>,
+		// header: () => <div className="text-left">Name</div>,
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "desc")}
+					className="text-left"
+				>
+					Name
+					<ArrowUpDown className="ml-2 h-4 w-4" />
+				</Button>
+			)
+		},
 		cell: ({row}) => {
-			return <div className="text-left">{capitalize(row.original.name)}</div>
+			return <div className="text-left px-5">{capitalize(row.original.name)}</div>
 		},
 	},
 	{
 		accessorKey: "exp",
-		header: () => <div className="text-left">Base Experience</div>,
+		// header: () => <div className="text-left">Base Experience</div>,
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "desc")}
+					className="text-left"
+				>
+					Base Experience
+					<ArrowUpDown className="ml-2 h-4 w-4" />
+				</Button>
+			)
+		},
 		cell: ({row}) => {
-			return <div className="text-left">{row.original.exp}</div>
+			return <div className="text-left px-5">{row.original.exp}</div>
 		},
 	},
 ]
